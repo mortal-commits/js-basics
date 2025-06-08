13b3ca5b145c864afb793dd8b5f9b49c18aaf253
Zaid Shaikh <sheikhzaid110@gmail.com>
2025-06-08 11:26:13 +0000

    About Datatypes.

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,38 @@
+"use strict";// This will treat whole program as newer version of JS.
+
+// alert("hello world"); // This is nodejs not browser there is different way to do it here
+
+/*
+console.log(
+3+
+3
+);
+// This is working but the code should be readable. This is not readable at all
+
+
+# There are two main documentations:
+    1. MDN
+    2. ECMA
+*/
+
+// Primitive Datatypes
+let name = "ben"; // String
+let age = 12; // number
+let decimal = 1.2 // float (which is also treated as number in js)
+let boolean = true // boolean (either true/false)
+// bigint (number but very big than normal number)
+//variable(container)
+//null (standalone value) # is not undefined 
+// undefined # the value is not defined 
+// symbol (used for defining uniqueness)
+
+//Non-primitive Datatypes
+//objects 
+// arrays
+// functions
+// classes
+
+console.log(typeof 'new');//typeof is used to know datatype of an element.
+
+let empty = null;
+console.log(typeof empty);//in js null is an object by default.
