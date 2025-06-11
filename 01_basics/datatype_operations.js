05263040e1ab4315256b38212563fcb59caf9085
Zaid Shaikh <sheikhzaid110@gmail.com>
2025-06-11 08:29:00 +0000

    about operations and exceptions

---
+++ 01_basics/datatype_operations.js
@@ -0,0 +1,30 @@
+// Video 7
+//      **** Basic Operations ****
+
+
+/*
+console.log(2+2);//addition
+console.log(2-2);//subtraction
+console.log(2*2);//multiplication
+console.log(2**3);//3 is power of two
+console.log(2/3);//division
+console.log(2%3);// to get the remainder for the division.
+*/
+
+//concatination of string
+let a = "hello";
+let b = " world";
+let c = a + b;
+//console.log(c); // output: hello world
+
+// coversion of string to number while concatination
+/*
+console.log("2" + 1);// output: 21
+console.log(2 + "1");// output: 21
+console.log("1" + 2 + 2);// output: 122 (converts everything to string)
+console.log(1 + "2" + 2);//output: 122 (converts everything to string)
+console.log(1 + 2 + "2");//output: 32 (first calculate the expression then add it to string)
+*/
+// if string is in the last position the first expression is evaluated
+let gameCounter = 0;
+gameCounter++;// increment operator(increment by 1);    
